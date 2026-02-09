bb1354a9c2b9805a72af6c34af685e5c7c391cba
Ian Swensson <ianswensson@Ians-MacBook-Pro.local>
2026-02-09 09:05:26 -0700

    overflow fix

--- src/config/router/routes/Root.tsx
+++ src/config/router/routes/Root.tsx
@@ -20,10 +20,10 @@ function Root() {
 
 
     return (
-        <div className="h-[100dvh] w-screen">
+        <div className="h-[100dvh] w-screen overflow-hidden">
             <div className="h-full w-full flex flex-col-reverse bottom-0 sm:flex-col">
                 <NavBar />
-                <div className="flex-grow flex overflow-hidden w-full justify-center z-10">
+                <div className="flex-grow flex overflow-y-auto w-full justify-center z-10">
                     <Outlet />
                 </div>
             </div>
